cef2f6cdfe0ba92b27e045f1a20fa6abcc2ec342
Emily <emilichi04@gmail.com>
2025-11-22 20:25:23 +0200

    created a list of books

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 # "ןורכיזב "םינותנ דסמ
-books = []
-counter = 1
-
+books = [
+    {"id": 111, "title": "The Hobbit", "author": "J.R.R. Tolkien", "year": 1937},
+    {"id": 222, "title": "Dune", "author": "Frank Herbert", "year": 1965},
+    {"id": 444, "title": "All the Light We Cannot See", "author": "Anthony Doerr", "year": 2014},
+    {"id": 555, "title": "The Shining", "author": "Stephen King", "year": 1977}
+]
+counter = 5
 
 @app.get("/books")
 def get_books():
@@ -25,7 +29,7 @@ def add_book(book: dict):
     global counter
     new_book = {
         "id": counter, "title": book.get("title"),
-        "author": book.get("author")
+        "author": book.get("author"), "year": book.get("year")
     }
     books.append(new_book)
     counter += 1
